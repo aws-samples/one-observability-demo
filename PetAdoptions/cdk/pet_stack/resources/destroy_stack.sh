8c12b1f3c6607f1a59ea2190de991668fd93963d
Imaya Kumar Jagannathan <ijaganna@amazon.com>
2020-08-10 15:27:15 -0400

    Clean up on destroy script

--- PetAdoptions/cdk/pet_stack/resources/destroy_stack.sh
+++ PetAdoptions/cdk/pet_stack/resources/destroy_stack.sh
@@ -11,19 +11,23 @@ aws dynamodb update-contributor-insights --table-name $DDB_CONTRIB --contributor
 # Fetch the name of the S3 bucket created by CDKToolkit for bootstrap
 CDK_S3_BUCKET_NAME=$(aws cloudformation describe-stacks  --stack-name CDKToolkit | jq '.Stacks[0].Outputs[] | select(.OutputKey == "BucketName").OutputValue' -r)
 
-# Empty the S3 bucket CDKToolkit created
+# Empty the S3 bucket CDKToolkit
+echo CLEANING OUT BOOTSTRAP S3 BUCKET CONTENTS
+echo -----------------------------------------
 aws s3 rm s3://$CDK_S3_BUCKET_NAME --recursive   
 
-# Delete resources such as S3 buckets etc createed by CDKToolkit
+# Delete resources such as S3 buckets etc created by CDKToolkit
+echo DELETING THE BOOTSTRAP S3 BUCKET
+echo ----------------------------------
 aws cloudformation delete-stack --stack-name CDKToolkit
 
-# Get the mail stack name
+# Get the main stack name
 STACK_NAME=$(aws ssm get-parameter --name '/petstore/stackname' --region $AWS_REGION | jq .Parameter.Value -r)
 
 # Get rid of all resources
 cdk destroy $STACK_NAME
 
-# Sometimes the Sqlseeder doesn't get deleted cleanly. This helps clean up the environment completely including Sqlseeder
+# Sometimes the SqlSeeder doesn't get deleted cleanly. This helps clean up the environment completely including Sqlseeder
 aws cloudformation delete-stack --stack-name $STACK_NAME
 
 echo ----- ✅ DONE --------
