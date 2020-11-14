11cd46fae340887944746f35f6bac6739e9c7d34
Rodrigue Koffi <koffirodrigue@gmail.com>
2020-11-14 23:03:18 +0000

    Add update availability after adoption

--- PetAdoptions/payforadoption-go/go.mod
+++ PetAdoptions/payforadoption-go/go.mod
@@ -3,6 +3,8 @@ module github.com/aws-samples/one-observability-demo/PetAdoptions/payforadoption
 go 1.14
 
 require (
+	github.com/denisenkom/go-mssqldb v0.9.0
+	github.com/dghubble/sling v1.3.0
 	github.com/go-kit/kit v0.10.0
 	github.com/gofrs/uuid v3.3.0+incompatible
 	github.com/gorilla/mux v1.8.0
--- PetAdoptions/payforadoption-go/service.go
+++ PetAdoptions/payforadoption-go/service.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
 	"time"
 
+	"github.com/dghubble/sling"
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/gofrs/uuid"
@@ -13,7 +17,7 @@ import (
 // links endpoints to transport
 type Service interface {
 	HealthCheck(ctx context.Context) (string, error)
-	CompleteAdoption(ctx context.Context, petId, petType string) (string, error)
+	CompleteAdoption(ctx context.Context, petId, petType string) (Adoption, error)
 }
 
 // object that handles the logic and complies with interface
@@ -32,31 +36,86 @@ func NewService(logger log.Logger, rep Repository) Service {
 
 // health check logic
 func (s service) HealthCheck(ctx context.Context) (string, error) {
-	logger := log.With(s.logger, "method", "HealthCheck")
-	logger.Log("health check", ctx)
 	return "alive", nil
 }
 
 // /api/completeadoption logic
-func (s service) CompleteAdoption(ctx context.Context, petId, petType string) (string, error) {
+func (s service) CompleteAdoption(ctx context.Context, petId, petType string) (Adoption, error) {
 	logger := log.With(s.logger, "method", "CompleteAdoption")
 
 	uuid, _ := uuid.NewV4()
-	tx := Transaction{
-		ID:           uuid.String(),
-		PetID:        petId,
-		AdoptionDate: time.Now(),
+	a := Adoption{
+		TransactionID: uuid.String(),
+		PetID:         petId,
+		PetType:       petType,
+		AdoptionDate:  time.Now(),
 	}
 
 	logger.Log(
 		"traceId", "xray Trace to retrieve",
-		"transaction", fmt.Sprintf("%#v", tx),
+		"transaction", fmt.Sprintf("%#v", a),
 	)
 
-	if err := s.repository.CreateTransaction(ctx, tx); err != nil {
+	if err := s.repository.CreateTransaction(ctx, a); err != nil {
 		level.Error(logger).Log("err", err)
-		return "tx creation failed", err
+		return Adoption{}, err
 	}
 
-	return "success", nil
+	return a, s.updateAvailability("https://s0b8q2ju3b.execute-api.eu-west-1.amazonaws.com/prod/", a)
+}
+
+func (s service) updateAvailability(updateAdoptionURL string, a Adoption) error {
+	logger := log.With(s.logger, "method", "updateAvailability")
+
+	errs := make(chan error)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+
+		defer wg.Done()
+		client := &http.Client{}
+
+		body := &completeAdoptionRequest{a.PetID, a.PetType}
+		req, _ := sling.New().Put(updateAdoptionURL).BodyJSON(body).Request()
+		resp, err := client.Do(req)
+		if err != nil {
+			level.Error(logger).Log("err", err)
+			errs <- err
+			return
+		}
+
+		defer resp.Body.Close()
+		if body, err := ioutil.ReadAll(resp.Body); err != nil {
+			level.Error(logger).Log("err", err)
+			errs <- err
+		} else {
+			sb := string(body)
+			fmt.Println(sb)
+		}
+		//log.Printf(sb)
+	}()
+
+	go func() {
+		_, err := http.Get("https://amazon.com")
+		if err != nil {
+			level.Error(logger).Log("err", err)
+			errs <- err
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(errs)
+	}()
+
+	// return the first error
+	for err := range errs {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
