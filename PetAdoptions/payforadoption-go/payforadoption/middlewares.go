ea29877fe684e823a12b7f89db802561ff39e36d
Rodrigue Koffi <koffirodrigue@gmail.com>
2020-11-21 23:03:20 +0000

    Add annotation and metadata to traces

---
+++ PetAdoptions/payforadoption-go/payforadoption/middlewares.go
@@ -0,0 +1,59 @@
+package payforadoption
+
+import (
+	"context"
+	"time"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/go-kit/kit/log"
+)
+
+type middleware struct {
+	logger log.Logger
+	Service
+}
+
+func NewInstrumenting(logger log.Logger, s Service) Service {
+
+	return &middleware{
+		logger:  logger,
+		Service: s,
+	}
+}
+
+func (mw *middleware) CompleteAdoption(ctx context.Context, petId, petType string) (a Adoption, err error) {
+	defer func(begin time.Time) {
+
+		segment := xray.GetSegment(ctx)
+
+		xray.AddAnnotation(ctx, "petId", petId)
+		xray.AddAnnotation(ctx, "petType", petType)
+		xray.AddMetadata(ctx, "timeTakenSeconds", time.Since(begin).Seconds())
+
+		mw.logger.Log(
+			"method", "CompleteAdoption",
+			"traceId", segment.TraceID,
+			"petId", petId,
+			"petType", petType,
+			"took", time.Since(begin),
+			"err", err)
+	}(time.Now())
+
+	return mw.Service.CompleteAdoption(ctx, petId, petType)
+}
+
+func (mw *middleware) CleanupAdoptions(ctx context.Context) (err error) {
+	defer func(begin time.Time) {
+
+		segment := xray.GetSegment(ctx)
+		xray.AddMetadata(ctx, "timeTakenSeconds", time.Since(begin).Seconds())
+
+		mw.logger.Log(
+			"method", "CleanupAdoptions",
+			"traceId", segment.TraceID,
+			"took", time.Since(begin),
+			"err", err)
+	}(time.Now())
+
+	return mw.Service.CleanupAdoptions(ctx)
+}
