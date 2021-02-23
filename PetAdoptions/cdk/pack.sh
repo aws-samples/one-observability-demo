88b819975c679d839ee649c1b595397548427cea
Rafael Pereyra <rapg@amazon.com>
2021-02-23 18:45:51 -0500

    Added PutUpdate lambda code

--- PetAdoptions/cdk/pack.sh
+++ PetAdoptions/cdk/pack.sh
@@ -7,6 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 rm -f ./assets/*
 
 cd ./pet_stack
-zip -r ../assets/SourceCode.zip . -x "node_modules/**" "cdk.out/*" "out/*" "resources/aws-distro-for-opentelemetry-python-38-preview.zip" ".vscode/**"
+zip -r ../assets/SourceCode.zip . -x "node_modules/**" "cdk.out/*" "out/*" "resources/aws-distro-for-opentelemetry-python-38-preview.zip" "resources/function.zip" ".vscode/**"
 cp resources/aws-distro-for-opentelemetry-python-38-preview.zip ../assets/
-cp ../../petsupdater/function.zip ../assets/
+cp resources/function.zip ../assets/
