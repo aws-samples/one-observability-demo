48ffc01569ca9d58eb3c60cc2652f3581750b9ca
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-02-24 12:15:46 +0100

    Fixed agent version instead of making it a moving target

--- PetAdoptions/petsearch-java/src/test/java/ca/petsearch/AWSTestConfig.java
+++ PetAdoptions/petsearch-java/src/test/java/ca/petsearch/AWSTestConfig.java
@@ -1,5 +1,6 @@
 package ca.petsearch.controllers;
 
+import ca.petsearch.WebConfig;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
@@ -8,6 +9,7 @@ import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 import static ca.petsearch.controllers.SearchControllerIT.localStack;
 import static org.testcontainers.containers.localstack.LocalStackContainer.Service.*;
@@ -16,6 +18,7 @@ import static org.testcontainers.containers.localstack.LocalStackContainer.Servi
 public class AWSTestConfig {
 
     @Bean
+    @Primary
     public AmazonS3 amazonS3() {
         return AmazonS3ClientBuilder.standard()
                 .withCredentials(localStack.getDefaultCredentialsProvider())
@@ -24,6 +27,7 @@ public class AWSTestConfig {
     }
 
     @Bean
+    @Primary
     public AmazonDynamoDB amazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
                 .withCredentials(localStack.getDefaultCredentialsProvider())
@@ -32,6 +36,7 @@ public class AWSTestConfig {
     }
 
     @Bean
+    @Primary
     public AWSSimpleSystemsManagement awsSimpleSystemsManagement() {
         return AWSSimpleSystemsManagementClientBuilder.standard()
                 .withCredentials(localStack.getDefaultCredentialsProvider())
