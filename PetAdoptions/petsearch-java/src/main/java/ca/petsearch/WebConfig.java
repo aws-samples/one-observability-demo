fd4432cffd1049e9ab1c97e7d7e3879b82b4a4f0
Fabio Braga de Oliveira <fabiool@amazon.com>
2021-02-24 17:11:21 +0100

    Add docker-compose to locally run the solution

--- PetAdoptions/petsearch-java/src/main/java/ca/petsearch/WebConfig.java
+++ PetAdoptions/petsearch-java/src/main/java/ca/petsearch/WebConfig.java
@@ -1,5 +1,6 @@
 package ca.petsearch;
 
+import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
@@ -8,7 +9,7 @@ import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement
 import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetryTracing;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +24,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final Tracer tracer;
 
+    @Value("${aws.local.endpoint:#{null}}")
+    private String endpoint = "";
+
+    @Value("${cloud.aws.region.static:#{null}}")
+    private String region = "";
+
     public WebConfig(Tracer tracer) {
         this.tracer = tracer;
     }
@@ -35,13 +42,15 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder.standard()
+        return endpoint.isEmpty() ? AmazonDynamoDBClientBuilder.standard()
+                .build() : AmazonDynamoDBClientBuilder.standard().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
                 .build();
     }
 
     @Bean
     public AWSSimpleSystemsManagement awsSimpleSystemsManagement() {
-        return AWSSimpleSystemsManagementClientBuilder.standard()
+        return endpoint.isEmpty() ? AWSSimpleSystemsManagementClientBuilder.standard()
+                .build() : AWSSimpleSystemsManagementClientBuilder.standard().withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
                 .build();
     }
 
