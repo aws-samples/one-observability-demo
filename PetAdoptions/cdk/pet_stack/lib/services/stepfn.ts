e81c938ad219f5b2de7a79f56f8dff5f3a4e8e75
Rafael Pereyra <rapg@amazon.com>
2021-04-15 09:39:35 -0400

    Updated Lambda Insights Layer to v12

--- PetAdoptions/cdk/pet_stack/lib/services/stepfn.ts
+++ PetAdoptions/cdk/pet_stack/lib/services/stepfn.ts
@@ -28,7 +28,7 @@ export class PetAdoptionsStepFn extends cdk.Construct {
       ]
     });
 
-    var layerArn = "arn:aws:lambda:" + process.env.CDK_DEFAULT_REGION + ":580247275435:layer:LambdaInsightsExtension:2";
+    var layerArn = "arn:aws:lambda:" + process.env.CDK_DEFAULT_REGION + ":580247275435:layer:LambdaInsightsExtension:12";
     //    var layerArn = "arn:aws:lambda:us-west-2:580247275435:layer:LambdaInsightsExtension:2";
     var layer = lambda.LayerVersion.fromLayerVersionArn(this, `LambdaInsights`, layerArn);
 
