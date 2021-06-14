55c8e1cde4d5b5e3f61ee9999fdb105c82c07a74
Rodrigue Koffi <koffir@amazon.com>
2021-06-14 16:05:04 +0000

    Update adot build resources

--- PetAdoptions/cdk/pet_stack/resources/build-adot-ecr.sh
+++ PetAdoptions/cdk/pet_stack/resources/build-adot-ecr.sh
@@ -37,11 +37,11 @@ receivers:
         scrape_interval: 15s
         scrape_timeout: 10s
       scrape_configs:
-      - job_name: "app"
+      - job_name: "test-prometheus-sample-app"
         static_configs:
         - targets: [ 0.0.0.0:80 ]
   awsecscontainermetrics:
-    collection_interval: 20s
+    collection_interval: 15s
 
 processors:
   filter:
@@ -51,20 +51,79 @@ processors:
         metric_names:
           - ecs.task.memory.utilized
           - ecs.task.memory.reserved
+          - ecs.task.memory.usage
           - ecs.task.cpu.utilized
           - ecs.task.cpu.reserved
+          - ecs.task.cpu.usage.vcpu
           - ecs.task.network.rate.rx
           - ecs.task.network.rate.tx
           - ecs.task.storage.read_bytes
           - ecs.task.storage.write_bytes
 
+  metricstransform:
+    transforms:
+      - metric_name: ecs.task.memory.utilized
+        action: update
+        new_name: MemoryUtilized
+      - metric_name: ecs.task.memory.reserved
+        action: update
+        new_name: MemoryReserved
+      - metric_name: ecs.task.memory.usage
+        action: update
+        new_name: MemoryUsage
+      - metric_name: ecs.task.cpu.utilized
+        action: update
+        new_name: CpuUtilized
+      - metric_name: ecs.task.cpu.reserved
+        action: update
+        new_name: CpuReserved
+      - metric_name: ecs.task.cpu.usage.vcpu
+        action: update
+        new_name: CpuUsage
+      - metric_name: ecs.task.network.rate.rx
+        action: update
+        new_name: NetworkRxBytes
+      - metric_name: ecs.task.network.rate.tx
+        action: update
+        new_name: NetworkTxBytes
+      - metric_name: ecs.task.storage.read_bytes
+        action: update
+        new_name: StorageReadBytes
+      - metric_name: ecs.task.storage.write_bytes
+        action: update
+        new_name: StorageWriteBytes
+
+  resource:
+    attributes:
+      - key: ClusterName
+        from_attribute: aws.ecs.cluster.name
+        action: insert
+      - key: aws.ecs.cluster.name
+        action: delete
+      - key: ServiceName
+        from_attribute: aws.ecs.service.name
+        action: insert
+      - key: aws.ecs.service.name
+        action: delete
+      - key: TaskId
+        from_attribute: aws.ecs.task.id
+        action: insert
+      - key: aws.ecs.task.id
+        action: delete
+      - key: TaskDefinitionFamily
+        from_attribute: aws.ecs.task.family
+        action: insert
+      - key: aws.ecs.task.family
+        action: delete
+
 exporters:
   awsprometheusremotewrite:
-    endpoint: https://aps-workspaces.eu-west-1.amazonaws.com/workspaces/$WORKSPACE_ID/api/v1/remote_write
+    endpoint: "https://aps-workspaces.$REGION.amazonaws.com/workspaces/$WORKSPACE_ID/api/v1/remote_write"
     aws_auth:
-      service: aps
       region: $REGION
-
+      service: "aps"
+    resource_to_telemetry_conversion:
+      enabled: true
   logging:
     loglevel: debug
 
