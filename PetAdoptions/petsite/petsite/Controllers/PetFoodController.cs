c9f69ede15d0965d1e0752d2b0ad2dda1a6d8039
Rich McDonough <mcdrich@amazon.com>
2022-01-16 09:07:23 -0500

    PetFoodController.cs now uses proper x-ray client for petfood calls

--- PetAdoptions/petsite/petsite/Controllers/PetFoodController.cs
+++ PetAdoptions/petsite/petsite/Controllers/PetFoodController.cs
@@ -1,32 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Net;
-using Amazon.XRay.Recorder.Handlers.AwsSdk;
-using Amazon.XRay.Recorder.Core;
-
-
-// unused imports
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using PetSite.Models;
 using System.Net.Http;
-using System.Web;
+using System.Threading.Tasks;
+using Amazon.XRay.Recorder.Core;
 using Amazon.XRay.Recorder.Handlers.System.Net;
-using System.Text.Json;
-using Amazon;
-using PetSite.ViewModels;
-using Microsoft.AspNetCore.Mvc.Rendering;
+using Amazon.XRay.Recorder.Handlers.AwsSdk;
 
-// todo: these need to use the proper x-ray-wrapped calls to petfood and petfood-metric. Right now these do not use the upstream trace ID
-// todo: move these, as per https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-dotnet-httpclients.html, to var httpClient = new HttpClient(new HttpClientXRayTracingHandler(new HttpClientHandler()));
+
 namespace PetSite.Controllers
 {
     public class PetFoodController : Controller
     {
+        
+        private static HttpClient httpClient;
+        private IConfiguration _configuration;
         
         public PetFoodController(IConfiguration configuration)
         
@@ -35,34 +23,39 @@ namespace PetSite.Controllers
         }
 
         [HttpGet("/petfood")]
-        public string PetFoodMethod()
+        public async Task<string> Index()
         {
+            // X-Ray FTW
             AWSXRayRecorder.Instance.BeginSubsegment("Calling PetFood");
             Console.WriteLine($"[{AWSXRayRecorder.Instance.GetEntity().TraceId}][{AWSXRayRecorder.Instance.TraceContext.GetEntity().RootSegment.TraceId}] - Calling PetFood");
-
-            WebClient client = new WebClient();
-            string downloadString = client.DownloadString("http://petfood");
+            
+            // Get our data from petfood
+            var httpClient = new HttpClient(new HttpClientXRayTracingHandler(new HttpClientHandler()));
+            string result = await httpClient.GetStringAsync("http://petfood");
+            
+            // Close the segment
             AWSXRayRecorder.Instance.EndSubsegment();
-
-            return downloadString;
+            
+            // Return the result!
+            return result;
         }
-
-        ///stubbing with simple http client for now, needs to have params pulled from URL path still
+        
         [HttpGet("/petfood-metric/{entityId}/{value}")]
-        public string PetFoodMetric(string entityId, float value)
+        public async Task<string> PetFoodMetric(string entityId, float value)
         {
+            // X-Ray FTW
             AWSXRayRecorder.Instance.BeginSubsegment("Calling PetFood metric");
-
-            WebClient client = new WebClient();
-            string downloadString = client.DownloadString("http://petfood-metric/metric/" + entityId + "/" + value.ToString());
             Console.WriteLine("Calling: " + "http://petfood-metric/metric/" + entityId + "/" + value.ToString());
+            Console.WriteLine($"[{AWSXRayRecorder.Instance.GetEntity().TraceId}][{AWSXRayRecorder.Instance.TraceContext.GetEntity().RootSegment.TraceId}] - Calling PetFood metric");            
+            
+            var httpClient = new HttpClient(new HttpClientXRayTracingHandler(new HttpClientHandler()));
+            string result = await httpClient.GetStringAsync("http://petfood-metric/metric/" + entityId + "/" + value.ToString());
 
-            Console.WriteLine($"[{AWSXRayRecorder.Instance.GetEntity().TraceId}][{AWSXRayRecorder.Instance.TraceContext.GetEntity().RootSegment.TraceId}] - Calling PetFood metric");
             AWSXRayRecorder.Instance.AddAnnotation("entityId", entityId);
             AWSXRayRecorder.Instance.AddAnnotation("value", value.ToString());
             AWSXRayRecorder.Instance.EndSubsegment();
             
-            return downloadString;
+            return result;
         }
 
     }
