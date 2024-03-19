5600abb8f37f8861e61bfbd5a938cf6edbc01295
Laurent Domb <ldo@amazon.com>
2024-03-18 21:00:14 -0400

    fixed version

--- PetAdoptions/petfood-metric/Dockerfile
+++ PetAdoptions/petfood-metric/Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 
-FROM python:3.8-slim-buster
+FROM --platform=linux/amd64 python:3.8-slim-buster
 
 WORKDIR /app
 
--- PetAdoptions/petfood/petfood.py
+++ PetAdoptions/petfood/petfood.py
@@ -1,5 +1,3 @@
-"""Simple microservice to show Evidently features"""
-
 import json
 import logging
 import os
@@ -9,7 +7,6 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 from aws_xray_sdk.core import patch_all, xray_recorder
 from flask import Flask, request
 
-
 app = Flask(__name__)
 plugins = ('EC2Plugin',)
 xray_recorder.configure(plugins=plugins, service='petfood')
@@ -17,21 +14,12 @@ patch_all()
 XRayMiddleware(app, xray_recorder)
 xray_recorder.begin_segment('petfood')
 
-
-class StructuredMessage:  # pylint: disable=R0903
-    """Use to make JSON formatted logging work well for CWL"""
-    def __init__(self, message, /, **kwargs):
-        self.message = message
-        self.kwargs = kwargs
-
-    def __str__(self):
-        return f'{self.message} - {self.kwargs}'
-
-
-_ = StructuredMessage
-logging.basicConfig(level=os.getenv('LOG_LEVEL', 20), format='%(message)s')
-logger = logging.getLogger()
-
+logging.basicConfig(
+    level=os.getenv('LOG_LEVEL', logging.INFO),
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
 
 class EvidentlyProject:
     """Base for all Evidently interactions"""
@@ -42,18 +30,18 @@ class EvidentlyProject:
         self.upsell_feature = 'petfood-upsell'
         self.upsell_text_feature = 'petfood-upsell-text'
 
-    @xray_recorder.capture('evidently project_exists')
+    @xray_recorder.capture('evidently_project_exists')
     def project_exists(self):
         """Returns False if the project does not currently exist"""
         try:
-            response = self.client.get_project(project=self.project)
-            logger.info(_('checking for evidently project', response=response))
+            self.client.get_project(project=self.project)
+            logger.info("Evidently project '%s' found", self.project)
             return True
         except self.client.exceptions.ResourceNotFoundException:
-            logger.warning(_('evidently project not found'))
-            return None
+            logger.warning("Evidently project '%s' not found", self.project)
+            return False
 
-    @xray_recorder.capture('evidently get_upsell_evaluation')
+    @xray_recorder.capture('evidently_get_upsell_evaluation')
     def get_upsell_evaluation(self, entity_id):
         """Gets the feature evaluation for petfood-upsell"""
         try:
@@ -67,10 +55,10 @@ class EvidentlyProject:
                 'variation': response['variation']
             }
         except self.client.exceptions.ResourceNotFoundException:
-            logger.warning(_('evidently feature ' + self.upsell_feature + ' not found for project'))
+            logger.warning("Evidently feature '%s' not found for project '%s'", self.upsell_feature, self.project)
             return return_default()
 
-    @xray_recorder.capture('evidently get_upsell_text')
+    @xray_recorder.capture('evidently_get_upsell_text')
     def get_upsell_text(self, entity_id):
         """Gets the feature evaluation for petfood-upsell-verbiage"""
         try:
@@ -79,59 +67,47 @@ class EvidentlyProject:
                 feature=self.upsell_text_feature,
                 project=self.project
             )
-            logger.info(_('evidently ' + self.upsell_text_feature, response=response))
+            logger.info("Evidently feature '%s': %s", self.upsell_text_feature, response['value']['stringValue'])
             return response['value']['stringValue']
         except self.client.exceptions.ResourceNotFoundException:
-            logger.warning(_('evidently feature ' + self.upsell_text_feature + ' not found for project'))
+            logger.warning("Evidently feature '%s' not found for project '%s'", self.upsell_text_feature, self.project)
             return 'Error getting upsell message - check that your feature exists in Evidently!'
-
 
 @xray_recorder.capture('return_evidently_response')
 def return_evidently_response(evidently):
     """Create a response using an Evidently project"""
-    logger.info(_('building evidently response'))
+    logger.info("Building Evidently response")
     entity_id = str(random.randint(1, 100))
     evaluation = evidently.get_upsell_evaluation(entity_id)
-    logger.warning(_('response from feature evaluation', evaluation=evaluation))
-    response = json.dumps(
-        {
-            'statusCode': 200,
-            'message': evidently.get_upsell_text(entity_id),
-            'variation': evaluation,
-            'entityId': entity_id
-        }
-    )
-    logger.warning(_('final response to request', response=response))
-    return response
-
+    logger.warning("Response from feature evaluation: %s", evaluation)
+    return json.dumps({
+        'statusCode': 200,
+        'message': evidently.get_upsell_text(entity_id),
+        'variation': evaluation,
+        'entityId': entity_id
+    })
 
 @xray_recorder.capture('return_default_response')
 def return_default():
     """Returns the default response to the user"""
-    logger.warning(_('returning default response to the user'))
-    text = json.dumps(
-        {
-            'message': 'Thank you for supporting our community!',
-            'statusCode': 200
-        }
-    )
-    return text
-
+    logger.warning("Returning default response to the user")
+    return json.dumps({
+        'message': 'Thank you for supporting our community!',
+        'statusCode': 200
+    })
 
 @app.route('/')
 def root_path():
     """Base URL for our handler"""
-    logger.info(_('raw request headers', headers=request.headers))
+    logger.info("Raw request headers: %s", request.headers)
     evidently = EvidentlyProject()
-    project = evidently.project_exists()
-    if not project:
+    if not evidently.project_exists():
         return return_default()
     else:
         return return_evidently_response(evidently)
 
-
 @app.route('/status')
 def status_path():
     """Used for health checks"""
-    logger.info(_('raw request headers', headers=request.headers))
+    logger.info("Raw request headers: %s", request.headers)
     return json.dumps({'statusCode': 200, 'body': 'ok'})
