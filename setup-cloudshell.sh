4bf10b7b2b19e67ed2ce93c827fedd2aea1352cb
Rodrigue Koffi <bonclay7@users.noreply.github.com>
2024-09-16 12:09:13 +0200

    Add idempotency

--- setup-cloudshell.sh
+++ setup-cloudshell.sh
@@ -5,17 +5,41 @@ if [ -z "$AWS_REGION" ]; then
 	exit 1
 fi
 
+function install_helm(){
+	echo "Installing helm"
+	curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+	chmod 700 get_helm.sh
+	./get_helm.sh
+	rm get_helm.sh
+}
+
+function install_eksctl(){
+	echo "Installing eksctl"
+	ARCH=amd64
+	PLATFORM=$(uname -s)_$ARCH
+	curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+	curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+	tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+	sudo mv /tmp/eksctl /usr/local/bin
+}
+
+function install_awscurl(){
+	echo "Installing awscurl"
+	pip install awscurl
+}
+
 cd /tmp
 
 # install helm
-echo "Installing helm"
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
-rm get_helm.sh
+command -v helm >/dev/null 2>&1 ||
+	{ install_helm; }
 
 # install awscurl
-echo "install awscurl"
-pip install awscurl
+command -v awscurl >/dev/null 2>&1 ||
+	{ install_awscurl; }
+
+# install eksctl
+command -v eksctl >/dev/null 2>&1 ||
+	{ install_eksctl; }
 
 cd -
