031492d259c1bfa79629864a35e34ece2c971a28
Rafael Pereyra <rapg@amazon.com>
2024-10-29 16:13:15 -0400

    Added self-mutating pipeline resources

--- PetAdoptions/cdk/pet_stack/app/pet_stack.ts
+++ PetAdoptions/cdk/pet_stack/app/pet_stack.ts
@@ -4,11 +4,17 @@ import { Services } from '../lib/services';
 import { Applications } from '../lib/applications';
 //import { EKSPetsite } from '../lib/ekspetsite'
 import { App, Tags, Aspects } from 'aws-cdk-lib';
+import { CDKPipeline } from '../lib/pipeline';
 //import { AwsSolutionsChecks } from 'cdk-nag';
 
 
 const stackName = "Services";
 const app = new App();
+
+const pipelineStack = new CDKPipeline(app, "Pipeline", {
+  sourceBucketName: process.env.SOURCE_BUCKET_NAME!,
+  branchName: process.env.GITHUB_BRANCH || "main"
+});
 
 const stack = new Services(app, stackName, { 
   env: { 
@@ -21,6 +27,8 @@ const applications = new Applications(app, "Applications", {
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION 
 }});
+
+
 
 Tags.of(app).add("Workshop","true")
 //Aspects.of(stack).add(new AwsSolutionsChecks({verbose: true}));
---
+++ PetAdoptions/cdk/pet_stack/lib/pipeline.ts
@@ -0,0 +1,30 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+export interface CDKPipelineProps extends cdk.StackProps {
+    sourceBucketName: string;
+    branchName: string;
+};
+
+export class CDKPipeline extends cdk.Stack {
+    constructor(scope: Construct, id: string, props: CDKPipelineProps) {
+        super(scope, id, props);
+
+        const sourceBucket = Bucket.fromBucketName(this, 'SourceBucket', props.sourceBucketName);
+
+        const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: 'PetAdoption',
+            synth: new ShellStep('SynthStep', {
+                input: CodePipelineSource.s3(sourceBucket,'Repository.zip'),
+                commands: [
+                        `cd one-observability-demo-${props.branchName}/PetAdoptions/cdk/pet_stack`,
+                        'npm install',
+                        'npm ci', 
+                        'npm run build', 
+                        'npx cdk synth']
+            })
+        });
+    }
+};
