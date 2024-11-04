3bd4e080235cc3e7bb498a822cd62945becfea37
Rafael Pereyra <rapg@amazon.com>
2024-11-04 12:14:29 -0500

    Bumped version of Aurora RDS Cluster

--- PetAdoptions/cdk/pet_stack/lib/services.ts
+++ PetAdoptions/cdk/pet_stack/lib/services.ts
@@ -129,7 +129,7 @@ export class Services extends Stack {
 
         const auroraCluster = new rds.ServerlessCluster(this, 'Database', {
 
-            engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_13_9 }),
+            engine: rds.DatabaseClusterEngine.auroraPostgres({ version: rds.AuroraPostgresEngineVersion.VER_13_15 }),
  
             parameterGroup: rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup', 'default.aurora-postgresql13'),
             vpc: theVPC,
