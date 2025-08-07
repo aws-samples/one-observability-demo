c77f0b69052f9780430ce2ffc2e7844d9885e941
Siva Guruvareddiar <gurusiva@amazon.com>
2025-08-06 22:16:17 -0500

    Clean up codebase: Remove unnecessary JS files and update gitignore
    - Remove generated JavaScript files from TypeScript compilation
    - Update .gitignore to exclude *.js files in the future
    - Keep only necessary TypeScript source files
    - Update list-adoptions-service.ts configuration
    - Update Dockerfile and start.sh for Python service

--- PetAdoptions/cdk/pet_stack/lib/services/list-adoptions-service.ts
+++ PetAdoptions/cdk/pet_stack/lib/services/list-adoptions-service.ts
@@ -18,7 +18,7 @@ export class ListAdoptionsService extends EcsService {
     props.database.secret?.grantRead(this.taskDefinition.taskRole);
     
     // Add environment variables for the Python service
-    this.container.addEnvironment('PORT', '8080');
+    this.container.addEnvironment('PORT', '80');
     this.container.addEnvironment('WORKERS', '4');
     this.container.addEnvironment('AWS_REGION', Stack.of(this).region);
   }
