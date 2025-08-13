00310419793d55c310e34bde8c903b0a2da7ab20
Kevin Lewin <lewinke@amazon.com>
2025-08-13 14:40:29 -0400

    fix: opensearch policy names

--- src/cdk/lib/constructs/opensearch-collection.ts
+++ src/cdk/lib/constructs/opensearch-collection.ts
@@ -2,8 +2,9 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-import { CfnOutput, Fn } from 'aws-cdk-lib';
+import { CfnOutput, Fn, Stack } from 'aws-cdk-lib';
 import { CfnCollection, CfnSecurityPolicy, CfnAccessPolicy } from 'aws-cdk-lib/aws-opensearchserverless';
+import { IRole } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 import {
     OPENSEARCH_COLLECTION_ARN_EXPORT_NAME,
@@ -33,6 +34,11 @@ export interface OpenSearchCollectionProperties {
      * @default 'TIMESERIES'
      */
     type?: 'SEARCH' | 'TIMESERIES' | 'VECTORSEARCH';
+    /**
+     * Roles that need access to ingest data into the OpenSearch collection
+     * @optional
+     */
+    ingestionRoles?: IRole[];
 }
 
 /**
@@ -68,13 +74,13 @@ export class OpenSearchCollection extends Construct {
     constructor(scope: Construct, id: string, properties?: OpenSearchCollectionProperties) {
         super(scope, id);
 
-        const collectionName = properties?.collectionName || 'petadoption-collection';
+        const collectionName = properties?.collectionName || 'pet-collection';
         const description = properties?.description || 'Pet adoption data collection';
         const type = properties?.type || 'TIMESERIES';
 
         // Create security policy for encryption
         this.securityPolicy = new CfnSecurityPolicy(this, 'SecurityPolicy', {
-            name: `${collectionName}-security-policy`,
+            name: `${collectionName}-sec-policy`,
             type: 'encryption',
             policy: JSON.stringify({
                 Rules: [
@@ -89,7 +95,7 @@ export class OpenSearchCollection extends Construct {
 
         // Create network security policy
         const networkSecurityPolicy = new CfnSecurityPolicy(this, 'NetworkSecurityPolicy', {
-            name: `${collectionName}-network-policy`,
+            name: `${collectionName}-net-policy`,
             type: 'network',
             policy: JSON.stringify([
                 {
@@ -103,7 +109,7 @@ export class OpenSearchCollection extends Construct {
                             Resource: [`collection/${collectionName}`],
                         },
                     ],
-                    AllowFromPublic: false,
+                    AllowFromPublic: true,
                 },
             ]),
         });
@@ -119,9 +125,15 @@ export class OpenSearchCollection extends Construct {
         this.collection.addDependency(this.securityPolicy);
         this.collection.addDependency(networkSecurityPolicy);
 
+        // Get the AWS account ID from the stack
+        const accountId = Stack.of(this).account;
+
+        // Allow all principals in the account to access the collection.
+        const principals: string[] = [`arn:aws:iam::${accountId}:root`];
+
         // Create access policy for the collection
         this.accessPolicy = new CfnAccessPolicy(this, 'AccessPolicy', {
-            name: `${collectionName}-access-policy`,
+            name: `${collectionName}-acc-policy`,
             type: 'data',
             policy: JSON.stringify([
                 {
@@ -149,7 +161,7 @@ export class OpenSearchCollection extends Construct {
                             ],
                         },
                     ],
-                    Principal: [`arn:aws:iam::*:root`],
+                    Principal: principals,
                 },
             ]),
         });
@@ -176,6 +188,12 @@ export class OpenSearchCollection extends Construct {
             value: this.collection.attrCollectionEndpoint,
             exportName: OPENSEARCH_COLLECTION_ENDPOINT_EXPORT_NAME,
         });
+
+        // Export access policy name for updates
+        new CfnOutput(this, 'AccessPolicyName', {
+            value: this.accessPolicy.name!,
+            exportName: `${OPENSEARCH_COLLECTION_ARN_EXPORT_NAME}-AccessPolicy`,
+        });
     }
 
     public static importFromExports(): {
@@ -192,6 +210,57 @@ export class OpenSearchCollection extends Construct {
             collectionId,
             collectionEndpoint,
         };
+    }
+
+    /**
+     * Add additional ingestion roles to the access policy
+     * @param roles - Array of IAM roles to grant access
+     */
+    public addIngestionRoles(roles: IRole[]): void {
+        const accountId = Stack.of(this).account;
+        const collectionName = this.collection.name;
+        
+        // Get current principals
+        const currentPolicy = JSON.parse(this.accessPolicy.policy as string);
+        const currentPrincipals = currentPolicy[0].Principal || [];
+        
+        // Add new role ARNs
+        roles.forEach(role => {
+            if (!currentPrincipals.includes(role.roleArn)) {
+                currentPrincipals.push(role.roleArn);
+            }
+        });
+        
+        // Update the access policy
+        this.accessPolicy.policy = JSON.stringify([
+            {
+                Rules: [
+                    {
+                        ResourceType: 'collection',
+                        Resource: [`collection/${collectionName}`],
+                        Permission: [
+                            'aoss:CreateCollectionItems',
+                            'aoss:DeleteCollectionItems',
+                            'aoss:UpdateCollectionItems',
+                            'aoss:DescribeCollectionItems',
+                        ],
+                    },
+                    {
+                        ResourceType: 'index',
+                        Resource: [`index/${collectionName}/*`],
+                        Permission: [
+                            'aoss:CreateIndex',
+                            'aoss:DeleteIndex',
+                            'aoss:UpdateIndex',
+                            'aoss:DescribeIndex',
+                            'aoss:ReadDocument',
+                            'aoss:WriteDocument',
+                        ],
+                    },
+                ],
+                Principal: currentPrincipals,
+            },
+        ]);
     }
 
     createOutputs(): void {
