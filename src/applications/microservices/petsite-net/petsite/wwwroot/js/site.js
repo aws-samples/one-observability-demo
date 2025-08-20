3d7e28beb02aa8df894b3b5ce8f6d8d35bc74fcb
Imaya Kumar Jagannathan <40274599+awsimaya@users.noreply.github.com>
2025-08-20 11:17:52 -0400

    Update gitignore to include missing css (#359)

--- src/applications/microservices/petsite-net/petsite/wwwroot/js/site.js
+++ src/applications/microservices/petsite-net/petsite/wwwroot/js/site.js
@@ -1,9 +1,4 @@
-/*
-Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-*/
-// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Placeholder for future JavaScript code
-console.log('Site JavaScript loaded');
+// Write your JavaScript code.
