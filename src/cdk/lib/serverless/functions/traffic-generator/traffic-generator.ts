b9db5e7bff75998fa2b26abaa2f91d59bdfb64f1
Siva Guruvareddiar <gurusiva@amazon.com>
2025-09-30 15:54:16 -0500

    traffic generator logic

--- src/cdk/lib/serverless/functions/traffic-generator/traffic-generator.ts
+++ src/cdk/lib/serverless/functions/traffic-generator/traffic-generator.ts
@@ -76,7 +76,7 @@ export class TrafficGeneratorFunction extends WokshopLambdaFunction {
     getBundling(): BundlingOptions {
         return {
             externalModules: [],
-            nodeModules: ['@aws-sdk/client-lambda'],
+            nodeModules: [],
         };
     }
 
