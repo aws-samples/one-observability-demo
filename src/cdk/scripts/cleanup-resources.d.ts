5ab00ba1d802ae5e8e64f1950018386012b52f50
Siva Guruvareddiar <gurusiva@amazon.com>
2025-10-01 17:59:27 -0500

    traffic generator logic and adding userId to list adoptions response

--- src/cdk/scripts/cleanup-resources.d.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ts-node
-export declare const TAGS: {
-    environment: string;
-    application: string;
-    stackName: string;
-};
--- src/cdk/scripts/utils/throttle-backoff.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Utility function for handling AWS throttling errors with exponential backoff
- * Retries API calls with increasing delays when throttling errors occur
- */
-export declare function throttlingBackOff<T>(callback: () => Promise<T>, maxRetries?: number, initialDelay?: number, maxDelay?: number): Promise<T>;
